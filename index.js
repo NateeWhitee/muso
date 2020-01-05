90fed579c565c57677ecdb588609d718f1e2d2ad
NateeWhitee <59496066+NateeWhitee@users.noreply.github.com>
2020-01-05 08:15:05 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ function showBandPage() {
 
 function showHighBrassPage() {
     changeTitle('<h1>High Brass Students</h1>');
-    changeText('<h1>Trumpet</h1><img src="trumpet.jpg"><br/><input type="button" value="find teachers near me!" onclick="getTeachersTrumpet()"><h1>French Horn</h1><img src="frenchHorn.png"><br/><input type="button" value="find teachers near me!" onclick="getTeachersFrenchHorn()">');
+    changeText('<h1>Trumpet</h1><img src="trumpet.jpg"><br/><br/><input type="button" value="find teachers near me!" onclick="getTeachersTrumpet()"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicTrumpet()"><h1>French Horn</h1><img src="frenchHorn.png"><br/><br/><input type="button" value="find teachers near me!" onclick="getTeachersFrenchHorn()"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicFrenchHorn()">');
     clearActiveBtnBackground();
     document.getElementById('showHighBrassPageBtn').setAttribute('class', 'active');
     bandInstrumentsNavBar.style.display = 'inline';
@@ -267,7 +267,7 @@ function showHighBrassPage() {
 
 function showLowBrassPage() {
     changeTitle('<h1>Low Brass Students</h1>');
-    changeText('<h1>Trombone</h1><img src="trombone.jpg"><br/><input type="button" value="find teachers near me!" onclick="getTeachersTrombone()"><h1>Baritone / Euphonium</h1><img src="baritone.jpg"><br/><input type="button" value="find teachers near me!" onclick="getTeachersBaritone()"><h1>Tuba</h1><img src="tuba.png"><br/><input type="button" value="find teachers near me!" onclick="getTeachersTuba()">');
+    changeText('<h1>Trombone</h1><img src="trombone.jpg"><br/><br/><input type="button" value="find teachers near me!" onclick="getTeachersTrombone()"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicTrombone()"><h1>Baritone / Euphonium</h1><img src="baritone.jpg"><br/><br/><input type="button" value="find teachers near me!" onclick="getTeachersBaritone()"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicBaritone()"><h1>Tuba</h1><img src="tuba.png"><br/><br/><input type="button" value="find teachers near me!" onclick="getTeachersTuba()"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicTuba()">');
     clearActiveBtnBackground();
     document.getElementById('showLowBrassPageBtn').setAttribute('class', 'active');
     bandInstrumentsNavBar.style.display = 'inline';
@@ -276,7 +276,7 @@ function showLowBrassPage() {
 
 function showHighReedsPage() {
     changeTitle('<h1>High Woodwind Students</h1>');
-    changeText('<h1>Piccolo / Flute</h1><img src="flute.jpg"><br/><input type="button" onclick="getTeachersFlute()" value="Find teachers near me!"></p><h1>Clarinet</h1><img src="clarinet.jpg"><br/><input type="button" onclick="getTeachersClarinet()" value="Find teachers near me!"><h1>Oboe</h1><img src="oboe.jpg"><br/><input type="button" onclick="getTeachersOboe()" value="Find teachers near me!"><h1>Alto Saxophone</h1><img src="altoSax.jpg"><br/><input type="button" onclick="getTeachersAltoSax()" value="Find teachers near me!"><h1>Tenor Saxophone</h1><img src="tenorSax.jfif"><br/><input type="button" onclick="getTeachersTenorSax()" value="Find teachers near me!">');
+    changeText('<h1>Piccolo / Flute</h1><img src="flute.jpg"><br/><br/><input type="button" onclick="getTeachersFlute()" value="Find teachers near me!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicFlute()"></p><h1>Clarinet</h1><img src="clarinet.jpg"><br/><br/><input type="button" onclick="getTeachersClarinet()" value="Find teachers near me!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicClarinet()"><h1>Oboe</h1><img src="oboe.jpg"><br/><br/><input type="button" onclick="getTeachersOboe()" value="Find teachers near me!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicOboe()"><h1>Alto Saxophone</h1><img src="altoSax.jpg"><br/><br/><input type="button" onclick="getTeachersAltoSax()" value="Find teachers near me!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicAltoSax()"><h1>Tenor Saxophone</h1><img src="tenorSax.jfif"><br/><br/><input type="button" onclick="getTeachersTenorSax()" value="Find teachers near me!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicTenorSax()">');
     clearActiveBtnBackground();
     document.getElementById('showHighReedsPageBtn').setAttribute('class', 'active');
     bandInstrumentsNavBar.style.display = 'inline';
@@ -285,7 +285,7 @@ function showHighReedsPage() {
 
 function showLowReedsPage() {
     changeTitle('<h1>Low Woodwind Students</h1>');
-    changeText('<h1>Baritone Saxophone</h1><img src="bariSax.jpg"><br/><input type="button" value="find teachers near me!" onclick="getTeachersBariSax()"><h1>Bass Clarinet</h1><img src="bassClarinet.jpg"><br/><input type="button" value="find teachers near me!" onclick="getTeachersBassClarinet()"><h1>Bassoon</h1><img src="bassoon.jpg"><br/><input type="button" value="find teachers near me!" onclick="getTeachersBassoon()">');
+    changeText('<h1>Baritone Saxophone</h1><img src="bariSax.jpg"><br/><br/><input type="button" value="find teachers near me!" onclick="getTeachersBariSax()"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicBariSax()"><h1>Bass Clarinet</h1><img src="bassClarinet.jpg"><br/><br/><input type="button" value="find teachers near me!" onclick="getTeachersBassClarinet()"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicBassClarinet()"><h1>Bassoon</h1><img src="bassoon.jpg"><br/><br/><input type="button" value="find teachers near me!" onclick="getTeachersBassoon()"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicBassoon()">');
     clearActiveBtnBackground();
     document.getElementById('showLowReedsPageBtn').setAttribute('class', 'active');
     bandInstrumentsNavBar.style.display = 'inline';
@@ -295,7 +295,7 @@ function showLowReedsPage() {
 function showOrchestraPage() {
     changeTitle('<h1>Orchestra Students</h1>');
     clearActiveBtnBackground();
-    changeText('<h1>Violin</h1><img src="violin.jpg"><br/><input type="button" onclick="getTeachersViolin()" value="find a teacher near you!"><h1>Viola</h1><img src="viola.webp"><br/><input type="button" onclick="getTeachersViola()" value="find a teacher near you!"><h1>Cello</h1><img src="cello.jpg"><br/><input type="button" onclick="getTeachersCello()" value="find a teacher near you!"><h1>String Bass</h1><img src="stringBass.jfif"><br/><input type="button" onclick="getTeachersStringBass()" value="find a teacher near you!">');
+    changeText('<h1>Violin</h1><img src="violin.jpg"><br/><br/><input type="button" onclick="getTeachersViolin()" value="find a teacher near you!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicViolin()"><h1>Viola</h1><img src="viola.webp"><br/><br/><input type="button" onclick="getTeachersViola()" value="find a teacher near you!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicViola()"><h1>Cello</h1><img src="cello.jpg"><br/><br/><input type="button" onclick="getTeachersCello()" value="find a teacher near you!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicCello()"><h1>String Bass</h1><img src="stringBass.jfif"><br/><br/><input type="button" onclick="getTeachersStringBass()" value="find a teacher near you!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicStringBass()">');
     document.getElementById('showOrchestraPageBtn').setAttribute('class', 'active');
     bandInstrumentsNavBar.style.display = 'none';
     console.log('display orchestra page');
@@ -303,7 +303,7 @@ function showOrchestraPage() {
 
 function showVocalPage() {
     changeTitle('<h1>Vocal Students</h1>');
-    changeText('<h1>Soprano</h1><img src="soprano.jpg"><br/><input type="button" onclick="getTeachersSoprano()" value="find a teacher near you!"><h1>Alto</h1><img src="alto.jfif"><br/><input type="button" onclick="getTeachersAlto()" value="find a teacher near you!"><h1>Tenor</h1><img src="tenor.jpg"><br/><input type="button" onclick="getTeachersTenor()" value="find a teacher near you!"><h1>Bass</h1><img src="bass.jpg"><br/><input type="button" onclick="getTeachersBass()" value="find a teacher near you!">');
+    changeText('<h1>Soprano</h1><img src="soprano.jpg"><br/><br/><input type="button" onclick="getTeachersSoprano()" value="find a teacher near you!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicSoprano()"><h1>Alto</h1><img src="alto.jfif"><br/><br/><input type="button" onclick="getTeachersAlto()" value="find a teacher near you!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicAlto()"><h1>Tenor</h1><img src="tenor.jpg"><br/><br/><input type="button" onclick="getTeachersTenor()" value="find a teacher near you!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicTenor()"><h1>Bass</h1><img src="bass.jpg"><br/><br/><input type="button" onclick="getTeachersBass()" value="find a teacher near you!"><input type="button" id="getSheetMusic" value="get playing exercises" onclick="getSheetMusicBass()">');
     clearActiveBtnBackground();
     document.getElementById('showVocalPageBtn').setAttribute('class', 'active');
     bandInstrumentsNavBar.style.display = 'none';
@@ -311,7 +311,7 @@ function showVocalPage() {
 }
 
 function showDataBaseOfTeachers() {
-    changeTitle('<h1>Database of Teachers</h1>');
+    changeTitle('<h1>All Teachers</h1>');
     changeText('<h1>Vocal</h1><h2>Soprano</h2><h3>City, State (There would be multiple of these)</h3><p>List of soprano teachers.</p><h2>Alto</h2><h3>City, State (There would be multiple of these)</h3><p>List of alto teachers</p><h2>Tenor</h2><h3>City, State (There would be multiple of these)</h3><p>List of tenor techers</p><h2>Bass</h2><h3>City, State (There would be multiple of these)</h3><p>List of bass teachers</p><h1>Orchestra</h1><h2>Violin</h2><h3>City, State (There would be multiple of these)</h3><p>List of violin teachers</p><h2>Viola</h2><h3>City, State (There would be multiple of these)</h3><p>List of viola teachers</p><h2>Cello</h2><h3>City, State (There would be multiple of these)</h3><p>List of cello teachers</p><h2>String Bass</h2><h3>City, State (There would be multiple of these)</h3><p>List of string base teachers</p><h1>Band</h1><h2>Trumpet</h2><h3>City, State (There would be multiple of these)</h3><p>List of trumpet teachers</p><h2>French Horn</h2><h3>City, State (There would be multiple of these)</h3><p>List of french horn teachers</p><h2>Baritone/Euphonium</h2><h3>City, State (There would be multiple of these)</h3><p>List of baritone/euphonium teachers</p><h2>Trombone</h2><h3>City, State (There would be multiple of these)</h3><p>List of trombone teachers</p><h2>Tuba</h2><h3>City, State (There would be multiple of these)</h3><p>List of tuba teachers</p>');
     clearActiveBtnBackground();
     document.getElementById('showDataBaseOfTeachers').setAttribute('class', 'active');
@@ -786,6 +786,167 @@ function getTeachersBass() {
             if (confirm('Select, "OK" to display the bass teachers in Nixa, MO')) {
                 changeText("<h1>List of Bass Teachers within Nixa, MO</h1>" + bassTeacherList.mo.nixa);
             }
+    }
+}
+
+// get sheet music
+// band
+// high brass
+function getSheetMusicTrumpet() {
+    if (confirm('Select, "OK" if you want to display the trumpet music exercices page.')) {
+        changeTitle('<h1>Trumpet: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicFrenchHorn() {
+    if (confirm('Select, "OK" if you want to display the french horn music exercices page.')) {
+        changeTitle('<h1>French Horn Music Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+
+// low brass
+function getSheetMusicTrombone() {
+    if (confirm('Select, "OK" if you want to display the trombone music exercices page.')) {
+        changeTitle('<h1>Trombone: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicBaritone() {
+    if (confirm('Select, "OK" if you want to display the baritone/euphonium music exercices page.')) {
+        changeTitle('<h1>Baritone/Euphonium: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicTuba() {
+    if (confirm('Select, "OK" if you want to display the tuba music exercices page.')) {
+        changeTitle('<h1>Tuba: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+
+// high woodwinds
+function getSheetMusicFlute() {
+    if (confirm('Select, "OK" if you want to display the flute music exercices page.')) {
+        changeTitle('<h1>Flute: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicClarinet() {
+    if (confirm('Select, "OK" if you want to display the clarinet music exercices page.')) {
+        changeTitle('<h1>Clarinet: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicOboe() {
+    if (confirm('Select, "OK" if you want to display the oboe music exercices page.')) {
+        changeTitle('<h1>Oboe: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicAltoSax() {
+    if (confirm('Select, "OK" if you want to display the alto saxophone music exercices page.')) {
+        changeTitle('<h1>Alto Saxophone: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicTenorSax() {
+    if (confirm('Select, "OK" if you want to display the tenor saxophone music exercices page.')) {
+        changeTitle('<h1>Tenor Saxophone: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+
+// low woodwinds
+function getSheetMusicBariSax() {
+    if (confirm('Select, "OK" if you want to display the baritone saxophone music exercices page.')) {
+        changeTitle('<h1>Baritone Saxophone: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicBassClarinet() {
+    if (confirm('Select, "OK" if you want to display the bass clarinet music exercices page.')) {
+        changeTitle('<h1>Bass Clarinet: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicBassoon() {
+    if (confirm('Select, "OK" if you want to display the bassoon music exercices page.')) {
+        changeTitle('<h1>Bassoon: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+
+// orchestra
+function getSheetMusicViolin() {
+    if (confirm('Select, "OK" if you want to display the violin music exercices page.')) {
+        changeTitle('<h1>Violin: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicViola() {
+    if (confirm('Select, "OK" if you want to display the viola music exercices page.')) {
+        changeTitle('<h1>Viola: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicCello() {
+    if (confirm('Select, "OK" if you want to display the cello music exercices page.')) {
+        changeTitle('<h1>Cello: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicStringBass() {
+    if (confirm('Select, "OK" if you want to display the string bass music exercices page.')) {
+        changeTitle('<h1>String Bass: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+
+// vocal
+function getSheetMusicSoprano() {
+    if (confirm('Select, "OK" if you want to display the soprano music exercices page.')) {
+        changeTitle('<h1>Soprano: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicAlto() {
+    if (confirm('Select, "OK" if you want to display the alto music exercices page.')) {
+        changeTitle('<h1>Alto: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicTenor() {
+    if (confirm('Select, "OK" if you want to display the tenor music exercices page.')) {
+        changeTitle('<h1>Tenor: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
+    }
+}
+function getSheetMusicBass() {
+    if (confirm('Select, "OK" if you want to display the bass music exercices page.')) {
+        changeTitle('<h1>Bass: Musical Exercices</h1>');
+        changeText('<h1>Technical Exercises</h1><h1>Lyrical Exercices</h1><h1>Long Tones</h1>');
+        clearActiveBtnBackground();
     }
 }
 
